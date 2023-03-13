c3eea7feb056e7ee95eaee44f4017fb17c367576
NgoanMC <ngoanmc@concrete-corp.com>
2023-03-13 14:30:03 +0700

    fix ssl

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://deliviet.mcntestsv.ga/wp-json/",
+  baseURL: "https://deliviet.mcntestsv.ga/wp-json/",
 });
 
 export default http;
