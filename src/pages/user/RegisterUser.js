7a812c0f6fce3de40fc774c80de511bdc5bcc5ad
nhanvm <minh.nhat.dev@gmail.com>
2023-03-30 17:00:37 +0700

    update register

--- src/pages/user/RegisterUser.js
+++ src/pages/user/RegisterUser.js
@@ -123,7 +123,7 @@ export default function RegisterUser() {
                           </p>
                         </div>
                       </div>
-                      <div class="-mx-3 mb-2">
+                      <div class="-mx-3 mb-4">
                         <div class="w-full px-3 mb-6 md:mb-0">
                           <label
                             class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -140,21 +140,23 @@ export default function RegisterUser() {
                               <option>Missouri</option>
                               <option>Texas</option>
                             </select>
-                            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                              <svg
-                                class="fill-current h-4 w-4"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                              >
-                                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                              </svg>
-                            </div>
                           </div>
                         </div>
                       </div>
-                      <div class="-mx-3 mb-2">
+                      <div class="flex items-center mb-4 -mx-3">
                         <div class="w-full px-3 mb-6 md:mb-0">
-                        <input type="checkbox" class="appearance-none checked:bg-gray-900 checked:border-transparent ..." />
+                          <input
+                            id="default-checkbox"
+                            type="checkbox"
+                            value=""
+                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                          />
+                          <label
+                            for="default-checkbox"
+                            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                          >
+                            Send the new user an email about their account.
+                          </label>
                         </div>
                       </div>
                     </form>
